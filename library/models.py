9b42e5ed8625c7c260eadc34ae1b39a62e2ad482
BasemGhoste <basemreiyad@gmail.com>
2025-01-19 00:07:49 +0200

    step 2

---
+++ library/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+    copies_available = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class BorrowRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} borrowed {self.book.title}"
---
+++ library/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from .models import Book, BorrowRecord
+from .serializers import BookSerializer, BorrowRecordSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BorrowRecordViewSet(viewsets.ModelViewSet):
+    queryset = BorrowRecord.objects.all()
+    serializer_class = BorrowRecordSerializer
